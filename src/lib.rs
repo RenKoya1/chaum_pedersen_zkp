26262a780fc42afa1dfea9c75b009aabc9e44fe3
RenKoya1 <koya.ren.701@gmail.com>
2023-09-04 23:31:28 +0400

    change test

--- src/lib.rs
+++ src/lib.rs
@@ -89,70 +89,73 @@ mod test {
   use super::*;
   #[test]
   
-  fn test_example(){
+  fn test() {
     let alpha = BigUint::from(4u32);
     let beta = BigUint::from(9u32);
     let p = BigUint::from(23u32);
     let q = BigUint::from(11u32);
-    let zkp = ZKP{
-      p:p.clone(),
-      q:q.clone(),  
-      alpha:alpha.clone(),
-      beta:beta.clone(),
+    let zkp = ZKP {
+        p: p.clone(),
+        q,
+        alpha: alpha.clone(),
+        beta: beta.clone(),
     };
+
     let x = BigUint::from(6u32);
     let k = BigUint::from(7u32);
+
     let c = BigUint::from(4u32);
-    
-    let y1 = ZKP::exponentiate(&alpha, &x, &p);
-    let y2 = ZKP::exponentiate(&beta, &x, &p);
-    
+
+    let (y1, y2) = zkp.compute_pair(&x);
     assert_eq!(y1, BigUint::from(2u32));
     assert_eq!(y2, BigUint::from(3u32));
-    
-    let r1 = ZKP::exponentiate(&alpha, &k, &p);
-    let r2 = ZKP::exponentiate(&beta, &k, &p);
-    
+
+    let (r1, r2) = zkp.compute_pair(&k);
     assert_eq!(r1, BigUint::from(8u32));
     assert_eq!(r2, BigUint::from(4u32));
-    
-    //fake secret
-    // let x_fake = BigUint::from(7u32);
-    let s = ZKP::solve(&zkp, &k, &c, &x);
+
+    let s = zkp.solve(&k, &c, &x);
     assert_eq!(s, BigUint::from(5u32));
-    
-    let result = ZKP::verify(&zkp, &r1, &r2, &y1, &y2,&c, &s);
+
+    let result = zkp.verify(&r1, &r2, &y1, &y2, &c, &s);
+    assert!(result);
+
+    // fake secret
+    let x_fake = BigUint::from(7u32);
+    let s_fake = zkp.solve(&k, &c, &x_fake);
+
+    let result = zkp.verify(&r1, &r2, &y1, &y2, &c, &s_fake);
     assert!(!result);
-  }
+}
+
 
   #[test]
-  fn test_example_with_random_numbers(){
+  fn test_with_random_numbers() {
     let alpha = BigUint::from(4u32);
     let beta = BigUint::from(9u32);
     let p = BigUint::from(23u32);
     let q = BigUint::from(11u32);
-    let zkp = ZKP{
-      p:p.clone(),
-      q:q.clone(),  
-      alpha:alpha.clone(),
-      beta:beta.clone(),
+    let zkp = ZKP {
+        p: p.clone(),
+        q: q.clone(),
+        alpha: alpha.clone(),
+        beta: beta.clone(),
     };
+
     let x = BigUint::from(6u32);
     let k = ZKP::generate_random_number_below(&q);
+
     let c = ZKP::generate_random_number_below(&q);
-    
-    let y1 = ZKP::exponentiate(&alpha, &x, &p);
-    let y2 = ZKP::exponentiate(&beta, &x, &p);
-    
+
+    let (y1, y2) = zkp.compute_pair(&x);
     assert_eq!(y1, BigUint::from(2u32));
     assert_eq!(y2, BigUint::from(3u32));
-    
-    let r1 = ZKP::exponentiate(&alpha, &k, &p);
-    let r2 = ZKP::exponentiate(&beta, &k, &p);
-    
-    let s = ZKP::solve(&zkp, &k, &c, &x);
-    
-    let result = ZKP::verify(&zkp, &r1, &r2, &y1, &y2,  &c, &s);
-    assert!(!result);
-  }
+
+    let (r1, r2) = zkp.compute_pair(&k);
+
+    let s = zkp.solve(&k, &c, &x);
+
+    let result = zkp.verify(&r1, &r2, &y1, &y2, &c, &s);
+    assert!(result);
+}
 }
